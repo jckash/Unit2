9c929ee9f775cd66766a89fa81ef9c23c454ee37
Jennifer Kash <jenniferkash3@gmail.com>
2023-08-28 01:06:56 -0400

    Updates

--- src/Order.java
+++ src/Order.java
@@ -1,6 +1,11 @@
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.time.LocalDate;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Order {
     public Order(ArrayList<Cupcake> cupcakeMenu, ArrayList<Drink> drinkMenu) {
@@ -72,7 +77,56 @@ public class Order {
                 if (!continueOrder.equalsIgnoreCase("y")) {
                     ordering = false;
                 }
+            }
 
+            System.out.println(order.get(0));
+            System.out.println(order.get(1));
+
+            double subTotal = 0.0;
+
+            for (int i = 2; i < order.size(); i++);
+            {
+                if (order.get(i).equals(cupcakeMenu.get(0))) {
+
+                    cupcakeMenu.get(0).type();
+                    System.out.println(cupcakeMenu.get(0).getPrice());
+                    subTotal = subTotal + cupcakeMenu.get(0).getPrice();
+
+                } else if (order.get(i).equals(cupcakeMenu.get(1))) {
+
+                    cupcakeMenu.get(1).type();
+                    System.out.println(cupcakeMenu.get(1).getPrice());
+                    subTotal = subTotal + cupcakeMenu.get(1).getPrice();
+
+                } else if (order.get(i).equals(cupcakeMenu.get(2))) {
+                    cupcakeMenu.get(2).type();
+                    System.out.println(cupcakeMenu.get(2).getPrice());
+                    subTotal = subTotal + cupcakeMenu.get(2).getPrice();
+
+                } else if (order.get(i).equals(drinkMenu.get(0))) {
+
+                    drinkMenu.get(0).type();
+                    System.out.println(drinkMenu.get(0).getPrice());
+                    subTotal = subTotal + drinkMenu.get(0).getPrice();
+
+                } else if (order.get(i).equals(drinkMenu.get(1))) {
+
+                    drinkMenu.get(1).type();
+                    System.out.println(drinkMenu.get(1).getPrice());
+                    subTotal = subTotal + drinkMenu.get(1).getPrice();
+
+                } else if (order.get(i).equals(drinkMenu.get(2))) {
+
+                    drinkMenu.get(2).type();
+                    System.out.println(drinkMenu.get(2).getPrice());
+                    subTotal = subTotal + drinkMenu.get(2).getPrice();
+
+                }
+            }
+            System.out.println("$" + subTotal + "\n");
+
+            new CreateFile();
+            new WriteToFile(order);
 
             } else {
                 System.out.println("Okay, well no soliciting. Have a nice day!");
@@ -81,5 +135,59 @@ public class Order {
 
         }
 
+    }
+
+class CreateFile
+{
+    public CreateFile()
+    {
+
+        try
+        {
+
+            File salesData = new File("salesData.txt");
+
+
+            if (salesData.createNewFile())
+            {
+                System.out.println("File created: " + salesData.getName());
+            }
+            else
+            {
+                System.out.println("File already exists");
+            }
+        }
+        catch (IOException e)
+        {
+            System.out.println("An error occurred");
+        }
+    }
+}
+
+
+
+class WriteToFile
+{
+    public WriteToFile(ArrayList<Object> order)
+    {
+
+        try
+        {
+            FileWriter fw = new FileWriter("salesData.txt", true);
+
+
+            PrintWriter salesWriter = new PrintWriter(fw);
+
+            for (int i = 0; i < order.size(); i++) {
+                salesWriter.println(order.get(i));
+            }
+            salesWriter.close();
+
+            System.out.println("Successfully wrote to the file");
+        }
+        catch (IOException e)
+        {
+            System.out.println("An error occurred");
+        }
     }
 }
